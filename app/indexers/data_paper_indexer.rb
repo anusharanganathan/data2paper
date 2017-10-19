f48adfa7c3ab2801b9d978e279e939af93c5107a
Anusha Ranganathan <anusha3@gmail.com>
2017-10-19 15:40:30 +0100

    Index data paper nested fields

--- app/indexers/data_paper_indexer.rb
+++ app/indexers/data_paper_indexer.rb
@@ -11,9 +11,26 @@ class DataPaperIndexer < Hyrax::WorkIndexer
 
 
   # Uncomment this block if you want to add custom indexing behavior:
-  # def generate_solr_document
-  #  super.tap do |solr_doc|
-  #    solr_doc['my_custom_field_ssim'] = object.my_custom_property
-  #  end
-  # end
+  def generate_solr_document
+    super.tap do |solr_doc|
+      # date
+      solr_doc[Solrizer.solr_name('date', :stored_searchable)] = object.date.map { |d| d.date.first }
+      solr_doc[Solrizer.solr_name('date', :displayable)] = object.date.to_json
+      object.date.each do |d|
+        label = DateTypesService.label(d.description.first) rescue nil
+        if label
+          solr_doc[Solrizer.solr_name("date_#{label.downcase}", :stored_sortable)] = d.date
+        end
+      end
+      # creator
+      creators = object.creator_nested.map { |c| (c.first_name + c.last_name).reject(&:blank?).join(' ') }
+      solr_doc[Solrizer.solr_name('creator_nested', :facetable)] = creators
+      solr_doc[Solrizer.solr_name('creator_nested', :stored_searchable)] = creators
+      solr_doc[Solrizer.solr_name('creator_nested', :displayable)] = object.creator_nested.to_json
+      # relation
+      solr_doc[Solrizer.solr_name('relation_url', :facetable)] = object.relation.map { |r| r.url.first }.reject(&:blank?)
+      solr_doc[Solrizer.solr_name('relation_id', :facetable)] = object.relation.map { |r| r.identifier.first }.reject(&:blank?)
+      solr_doc[Solrizer.solr_name('relation', :displayable)] = object.relation.to_json
+    end
+  end
 end
