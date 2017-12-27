4db92e071cea3f8cb24907dd46c3d4d1b86c14dd
Anusha Ranganathan <anusha3@gmail.com>
2017-12-27 11:34:51 +0000

    Generated journal work type

--- config/initializers/hyrax.rb
+++ config/initializers/hyrax.rb
@@ -1,6 +1,8 @@
 Hyrax.config do |config|
   # Injected via `rails g hyrax:work DataPaper`
   config.register_curation_concern :data_paper
+  # Injected via `rails g hyrax:work Journal`
+  config.register_curation_concern :journal
   # Register roles that are expected by your implementation.
   # @see Hyrax::RoleRegistry for additional details.
   # @note there are magical roles as defined in Hyrax::RoleRegistry::MAGIC_ROLES
