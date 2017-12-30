26daf51edb6fab3733d0777fbe19da0ee8c2da92
Anusha Ranganathan <anusha3@gmail.com>
2017-12-30 16:02:29 +0000

    issue #3 set correct option and hide visibility as visibility options will not be modified and user need not set this option

--- app/controllers/hyrax/journals_controller.rb
+++ app/controllers/hyrax/journals_controller.rb
@@ -10,5 +10,10 @@ module Hyrax
 
     # Use this line if you want to use a custom presenter
     self.show_presenter = Hyrax::JournalPresenter
+
+    def new
+      super
+      curation_concern.visibility = Hydra::AccessControls::AccessRight::VISIBILITY_TEXT_VALUE_PUBLIC
+    end
   end
 end
