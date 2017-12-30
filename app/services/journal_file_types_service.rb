88ac1d4c167e3dc494031afcb12ba2e06fb9cd1a
Anusha Ranganathan <anusha3@gmail.com>
2017-12-30 20:29:51 +0000

    Added file types authorities

---
+++ app/services/journal_file_types_service.rb
@@ -0,0 +1,14 @@
+module JournalFileTypesService
+  mattr_accessor :authority
+  self.authority = Qa::Authorities::Local.subauthority_for('journal_file_types')
+
+  def self.select_all_options
+    authority.all.map do |element|
+      [element[:label], element[:id]]
+    end
+  end
+
+  def self.label(id)
+    authority.find(id).fetch('term')
+  end
+end
