e98bf9bac13feec56d83da419d151e58b0d588fb
Anusha Ranganathan <anusha3@gmail.com>
2018-01-09 23:36:14 +0000

    Redirect user to appropriate page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,6 @@
 class ApplicationController < ActionController::Base
+  before_action :store_user_location!, if: :storable_location?
+
   helper Openseadragon::OpenseadragonHelper
   # Adds a few additional behaviors into the application controller
   include Blacklight::Controller
@@ -7,17 +9,41 @@ class ApplicationController < ActionController::Base
   # Adds Hyrax behaviors into the application controller
   include Hyrax::Controller
   include Hyrax::ThemedLayoutController
+
+  include Hyrax::DashboardHelperBehavior
+
   with_themed_layout '1_column'
 
-
   protect_from_forgery with: :exception
-
+  
   # Override Devise method to redirect to dashboard after signing in
-  def after_sign_in_path_for(_resource)
-    if request.env.has_key?('omniauth.origin') and request.env['omniauth.origin']
+  def after_sign_in_path_for(resource_or_scope)
+    if number_of_works(current_user) > 2
+      new_path = '/dashboard'
+    else
+      new_path = '/'
+    end
+    if request.env.has_key?('omniauth.origin') and 
+      request.env['omniauth.origin'].present? and request.env['omniauth.origin'] != '/'
       request.env['omniauth.origin']
     else
-      '/'
+      stored_location_for(resource_or_scope) || new_path
     end
   end
+
+
+  private
+    # Its important that the location is NOT stored if:
+    # - The request method is not GET (non idempotent)
+    # - The request is handled by a Devise controller such as Devise::SessionsController as that could cause an 
+    #    infinite redirect loop.
+    # - The request is an Ajax request as this can lead to very unexpected behaviour.
+    def storable_location?
+      request.get? && is_navigational_format? && !devise_controller? && !request.xhr? 
+    end
+
+    def store_user_location!
+      # :user is the scope we are authenticating
+      store_location_for(:user, request.fullpath)
+    end
 end
