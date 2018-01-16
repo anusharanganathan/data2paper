08b3778f082ea164a8c35d029befdbb626f6bc26
Anusha Ranganathan <anusha3@gmail.com>
2018-01-16 15:35:45 +0000

    Add default status to data paper and no edits once submitted

--- app/controllers/hyrax/data_papers_controller.rb
+++ app/controllers/hyrax/data_papers_controller.rb
@@ -11,7 +11,18 @@ module Hyrax
     # Use this line if you want to use a custom presenter
     self.show_presenter = Hyrax::DataPaperPresenter
 
-    # TODO: Submit data paper tojournal
+    def new
+      curation_concern.status = 'new'
+      super
+    end
+
+    def edit
+      raise CanCan::AccessDenied.new("Not authorized!", :edit, curation_concern) unless curation_concern.status_allows_edit?
+      curation_concern.status = 'draft'
+      super
+    end
+
+    # TODO: Submit data paper to journal
     #   Throw error if no user email or data paper or data paper journal or data paper journal account service email 
     #   To deliver email: ElsevierMailer.submission_email(user, data_paper).deliver
   end
