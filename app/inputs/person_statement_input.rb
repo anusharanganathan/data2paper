0960a57b025305e6eacf41cb8d32a16de8b9f7ac
Anusha Ranganathan <anusha3@gmail.com>
2018-01-17 00:05:41 +0000

    Orcid not to be manually filled

--- app/inputs/person_statement_input.rb
+++ app/inputs/person_statement_input.rb
@@ -30,23 +30,6 @@ protected
     out << '  </div>'
     out << '</div>' # row
 
-    # --- orcid
-    field = :orcid
-    field_name = name_for(attribute_name, index, field)
-    field_id = id_for(attribute_name, index, field)
-    field_value = person_statement.send(field).first
-
-    out << "<div class='row'>"
-    out << "  <div class='col-md-3'>"
-    out << template.label_tag(field_name, field.to_s.humanize, required: false)
-    out << '  </div>'
-
-    out << "  <div class='col-md-9'>"
-    out << @builder.text_field(field_name,
-        options.merge(value: field_value, name: field_name, id: field_id, required: false))
-    out << '  </div>'
-    out << '</div>' # row
-
     # --- affiliation
     field = :affiliation
     field_name = name_for(attribute_name, index, field)
