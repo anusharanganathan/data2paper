6b325be162557dc9bd95439c1b568d9915ea4d09
Martyn Whitwell <martyn.whitwell@gmail.com>
2018-02-23 12:11:45 +0000

    Fix fedora data volume issue

--- fedora_commons/bin/docker-entrypoint.sh
+++ fedora_commons/bin/docker-entrypoint.sh
@@ -8,16 +8,21 @@ if [[ "$VERBOSE" = "yes" ]]; then
     set -x
 fi
 
-ln -sf $APP_CONTAINER/bin $APP_LOCAL_VOLUME/
-ln -sf $APP_CONTAINER/conf $APP_LOCAL_VOLUME/
-ln -sf $APP_CONTAINER/lib $APP_LOCAL_VOLUME/
+
+# For this setup, it is easier to make the entire CATALINA_BASE a volume. That means that when the volume is mounted at container
+# start-up, everything done inside CATALINA_BASE in this Dockerfile is going to disappear (unless it's being run on a
+# local developer workstation). All actions that need to be taken inside CATALINA_BASE should be performed in this file.
+
+ln -sf $APP_STATIC/bin $APP_HOME/
+ln -sf $APP_STATIC/conf $APP_HOME/
 
 
+mkdir -p $APP_HOME/logs $APP_HOME/webapps $APP_HOME/work $APP_HOME/temp
 
-mkdir -p $APP_LOCAL_VOLUME/logs $APP_LOCAL_VOLUME/webapps $APP_LOCAL_VOLUME/work $APP_LOCAL_VOLUME/temp
+ln -sf $APP_STATIC/webapps/ROOT.war $APP_HOME/webapps/ROOT.war
 
-ln -sf $APP_CONTAINER/webapps/ROOT.war $APP_LOCAL_VOLUME/webapps/ROOT.war
-
+# Wait for Postgres
+wait_for_postgres.sh
 
 
 # start TomCat in foreground mode
