c16f0a7f3e21fcabb361089d7bbccd91d9e27795
Anusha Ranganathan <anusha3@gmail.com>
2018-03-03 10:16:14 +0000

    Read data from env vars

--- config/initializers/hyrax.rb
+++ config/initializers/hyrax.rb
@@ -82,7 +82,7 @@ Hyrax.config do |config|
   # config.redis_namespace = "hyrax"
 
   # Path to the file characterization tool
-  config.fits_path = "/home/appuser/fits-1.0.5/fits.sh"
+  config.fits_path = ENV['FITS_PATH'] || "/home/data2paper/fits-1.0.5/fits.sh"
 
   # Path to the file derivatives creation tool
   # config.libreoffice_path = "soffice"
