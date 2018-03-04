a34bf6275464d9e6ae938d76b65bb9621753f5f0
Anusha Ranganathan <anusha3@gmail.com>
2018-03-04 18:04:27 +0000

    Add file types with file upload and save

--- app/actors/hyrax/actors/create_with_files_and_types_actor.rb
+++ app/actors/hyrax/actors/create_with_files_and_types_actor.rb
@@ -2,14 +2,36 @@ module Hyrax
   module Actors
     class CreateWithFilesAndTypesActor < Hyrax::Actors::CreateWithFilesActor
       def create(env)
-        uploaded_file_types = filter_file_types(env.attributes.delete(:uploaded_file_types))
-        super
+        file_types = filter_file_types(env.attributes.delete(:uploaded_file_types))
+        uploaded_file_ids = filter_file_ids(env.attributes.delete(:uploaded_files))
+        files = uploaded_files(uploaded_file_ids)
+        validate_files(files, env) && next_actor.create(env) && attach_files(files, file_types, env)
+      end
+
+      def update(env)
+        file_types = filter_file_types(env.attributes.delete(:uploaded_file_types))
+        uploaded_file_ids = filter_file_ids(env.attributes.delete(:uploaded_files))
+        files = uploaded_files(uploaded_file_ids)
+        validate_files(files, env) && next_actor.update(env) && attach_files(files, file_types, env)
       end
 
       private 
+        # @return [TrueClass]
+        def attach_files(files, file_types, env)
+          return true if files.blank?
+          AttachFilesWithTypeToWorkJob.perform_later(env.curation_concern, files, file_types, env.attributes.to_h.symbolize_keys)
+          true
+        end
+
 
         def filter_file_types(input)
-          input.select {|_,v| v.present? }
+          file_types = {}
+          input.values.each do |val|
+            if val.fetch('file_id', []).first
+              file_types[val.fetch('file_id', []).first] = val.fetch('file_type', []).first
+            end
+          end
+          file_types.delete_if { |k, v| v.blank? }.with_indifferent_access
         end
 
     end
--- app/assets/javascripts/file_add_type.js
+++ app/assets/javascripts/file_add_type.js
@@ -1,15 +1,24 @@
 Blacklight.onLoad(function() {
   $('#fileupload').bind('fileuploadcompleted', function (e, data) {
-    var ele = $('#file_resource_type > select').clone().removeAttr('id');
-    $('.template-download').each(function() {
-      if ($(this).children('td').length < 5) {
-        var file_name = $(this).find(".name > input").val();
-        var input_name = "uploaded_file_types[" + file_name + "]";
-        ele.attr('name', input_name);
-        $td = $('<td/>').append(ele);
-        $(this).append($td);
-      }
-    });
+    window.setTimeout(add_type, 500);
   });
 });
 
+
+function add_type() {
+  $('.template-download').each(function(index) {
+    var ele = $('#file_resource_type > select').clone().removeAttr('id');
+    if ($(this).children('td').length < 5) {
+      var file_name = $(this).find(".name > input").val();
+      var input_name = "data_paper[uploaded_file_types][" + index + "][file_type][]";
+      ele.attr('name', input_name);
+      var ele2 = $('<input>').attr({
+        type: 'hidden',
+        name: "data_paper[uploaded_file_types][" + index + "][file_id][]"
+      })
+      ele2.val(file_name);
+      $td = $('<td/>').append(ele2).append(ele);
+      $(this).append($td);
+    }
+  });
+}
